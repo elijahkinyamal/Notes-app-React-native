7506d4f15c2b227b3c94d51c0fed95bebec1f038
ELIJAH Kinyamal <94557540+elijahkinyamal@users.noreply.github.com>
2024-02-05 21:36:48 +0300

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <View  style = {{margin:15}}>
-      <Text style = {{ fontWeight:'bold', fontSize:30, color:'white'}}>
+    <View  style = {{margin:16}}>
+      <Text style = {{ fontWeight:'bold', fontSize:31, color:'white'}}>
         Header
       </Text>
     </View>
